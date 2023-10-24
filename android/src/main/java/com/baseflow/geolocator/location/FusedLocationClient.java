dcd0eb3c7cdc8c788684f187d67f35b30ae2ed3d
Mahfuz Jailani Ibrahim <mahfuz.ibrahim@segari.id>
2023-10-24 15:35:19 +0700

    f

--- android/src/main/java/com/baseflow/geolocator/location/FusedLocationClient.java
+++ android/src/main/java/com/baseflow/geolocator/location/FusedLocationClient.java
@@ -14,6 +14,7 @@ import com.baseflow.geolocator.errors.ErrorCodes;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.*;
+import android.location.LocationRequest;
 import java.util.Random;
 
 class FusedLocationClient implements LocationClient {
